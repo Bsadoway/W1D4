7686588356f2032f41d152d500069f5bb60c13eb
Blake Sadoway <blake.sadoway@gmail.com>
2017-11-30 23:04:59 +0000

    counting down function added

--- loaded-die.js
+++ loaded-die.js
@@ -14,3 +14,32 @@ var rollLoadedDie = makeLoadedDie();
 console.log(rollLoadedDie());  // 5
 console.log(rollLoadedDie());  // 4
 console.log(rollLoadedDie());  // 6
+
+
+var countdownGenerator = function (x) {
+  var countdownNum = x;
+
+  return function(){
+    var tempCountdown = countdownNum;
+    var text = '';
+    if(tempCountdown > 0){
+      text = "T-minus " + tempCountdown + "...";
+    }
+    else if(tempCountdown === 0){
+      text = "Blast Off!"
+    }
+    else {
+      text = "Rockets already gone, bub!";
+    }
+    countdownNum--;
+    console.log(text);
+  }
+};
+
+var countdown = countdownGenerator(3);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
